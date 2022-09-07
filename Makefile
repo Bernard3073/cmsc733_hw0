7c80c2478cb59d6836121ebdb6eb46f9cb9c36b1
Bo-Shiang <bert41003@gmail.com>
2022-09-07 08:46:29 -0400

    first commit

---
+++ Makefile
@@ -0,0 +1,7 @@
+CC = g++
+PROJECT = main
+SRC = main.cpp
+LIBS = `pkg-config --cflags --libs opencv4`
+$(PROJECT) : $(SRC)
+	$(CC) $(SRC) -o $(PROJECT) $(LIBS)
+
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <Eigen/Dense>
+
+int main(){
+    // image show test
+    // cv::Mat img;
+    // img = cv::imread("/home/bo/CMSC733/hw0/brendan_iribe_center.jpg", cv::IMREAD_COLOR);
+    // if(! img.data)
+    // {
+    //     std::cout<<"Could not open file" << std::endl;
+    //     return -1;
+    // }
+    // cv::imshow("irb", img);
+    // cv::waitKey(0);
+    
+    // Eigen test
+    // Eigen::MatrixXd A(2,2);
+    // A << 1,2,3,4;
+    // std::cout << A << std::endl;
+    return 0;
+}
